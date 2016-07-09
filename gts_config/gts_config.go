c5d418c5aadda2be0e5e02a148ba1fdcdbcfca01
Mingcai SHEN <archsh@gmail.com>
2016-07-09 09:14:20 +0800

    Updated.

--- gts_config/gts_config.go
+++ gts_config/gts_config.go
@@ -1,54 +1,68 @@
 package gts_config
 
+import (
+	"github.com/BurntSushi/toml"
+)
+
 type LoggingConfig struct {
 	Filename string
-	Level string
-	Format string
+	Level    string
+	Format   string
 }
 
 type ServerConfig struct {
-	Listen string
-	Group string
-	User string
+	Listen  string
+	Group   string
+	User    string
 	Logging *LoggingConfig
 }
 
 type CacheConfig struct {
-	Id string
-	Size Size
+	Id      string
+	Size    Size
 	Expires Duration
 	Storage string
 }
 
 type BackendServerConfig struct {
-	Server string
-	Weight uint
-	Check bool
-	Backup bool
+	Server   string
+	Weight   uint
+	Check    bool
+	Backup   bool
 	Interval Duration
 }
 
 type BackendConfig struct {
-	Id string
+	Id      string
 	Servers []*BackendServerConfig
 }
 
 type PatternConfig struct {
-	Pattern Regex
-	Backends string
-	Caches string
+	Pattern   Regex
+	Backends  string
+	Caches    string
 	Cache_key string
-	Expires Duration
+	Expires   Duration
 }
 
 type FrontendConfig struct {
-	Id string
+	Id     string
 	Listen string
 }
 
 type GTS_Config struct {
-	Server *ServerConfig
-	Backends []*BackendConfig
-	Caches []*CacheConfig
+	Server    *ServerConfig
+	Backends  []*BackendConfig
+	Caches    []*CacheConfig
 	Frontends []*FrontendConfig
+}
+
+func LoadConfig(filename string, default_config *GTS_Config) (config *GTS_Config, err error) {
+	err = nil
+	config = new(GTS_Config)
+	if _, e := toml.DecodeFile(filename, config); nil != e {
+		err = e
+		return
+	}
+	return
 }
--- gtsd.go
+++ gtsd.go
@@ -1,9 +1,56 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"gts/gts_config"
+	"os"
 )
 
+const VERSION = "0.0.1"
+
 func main() {
-	fmt.Println("GTS, Another Traffic Server.")
+	var configFile string
+	var debugMode, daemonMode, checkConf, showHelp, showVersion bool
+	flag.StringVar(&configFile, "c", "/etc/gts/gts.conf", "Configuration filename.")
+	flag.BoolVar(&debugMode, "D", false, "Debug mode.")
+	flag.BoolVar(&daemonMode, "d", false, "Daemon mode.")
+	flag.BoolVar(&checkConf, "C", false, "Check Configuration only.")
+	flag.BoolVar(&showHelp, "h", false, "Show help.")
+	flag.BoolVar(&showVersion, "v", false, "Show version.")
+	flag.Parse()
+
+	os.Stderr.Write([]byte(fmt.Sprintf("GTS %v - Another Traffic Server.\n", VERSION)))
+	os.Stderr.Write([]byte("Copyright (C) 2016 Mingcai SHEN.\n"))
+	if showVersion {
+		os.Exit(0)
+	}
+
+	if showHelp {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	g_config, err := gts_config.LoadConfig(configFile, nil)
+	if nil != err {
+		os.Stderr.Write([]byte(fmt.Sprintf("Configuration(%s) Invalid <%s> ! \n", configFile, err)))
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if checkConf {
+
+		fmt.Printf("Configuration(%s) valid!\n", configFile)
+		fmt.Println("Configurations: ")
+		out, err := json.MarshalIndent(g_config, "", " ")
+		if nil != err {
+			fmt.Errorf("Failed!\n")
+		} else {
+			fmt.Println(string(out))
+
+		}
+		os.Exit(0)
+	}
+
 }
